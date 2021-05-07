a0b0288d984ec23dd64e077fb25a4ac1941f735d
Alpha Barry <barrymamadoualpha2@gmail.com>
2021-05-07 21:44:28 +0200

    =modification du formulair de location et category ajout de la validation

--- assets/js/pages/Registration.jsx
+++ assets/js/pages/Registration.jsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Alert from '../components/Alert';
+import Field from "../form/Field";
+import {Link} from "react-router-dom";
 
-const Registration = (props) => {
+const Registration = ({firstName,lastName, email,avatar}) => {
+    const [ users, setUsers] = useState({
+        firstName:"",
+        lastName:"",
+        email:"",
+        avatar:""
+    });
+    const [ errors, setErrors] = useState({
+        firstName:"",
+        lastName:"",
+        email:"",
+        avatar:""
+    });
+    const  handleChange=({currentTarget})=>{
+
+    }
+    const handleSubmit = (event)=>{
+       console.log("handleSubmit")
+    }
     return (<>
 
+            <form onSubmit={handleSubmit}>
+                <Field name="firstName"
+                       label="Cognome del'uttente" placeholder="Nome del'uttente"
+                       onChange={handleChange}
+                       value={users.firstName}
+                       error={errors.firstName}
+                />
+                <Field name="lastName"
+                       label="Nome del'uttente" placeholder="cognome del'uttente"
+                       onChange={handleChange}
+                       value={users.firstName}
+                       error={errors.firstName}
+                />
+                <Field name="email"
+                       label="email" placeholder="indirizzio email"
+                       onChange={handleChange}
+                       value={users.firstName}
+                       error={errors.firstName}
+                />
+                <button type="submit" className="btn btn-outline-success ">Mi Registro</button>
+                <Link to="/login"><button className="btn btn-outline-success ">mi collego</button>
+                </Link>
+        </form>
         <Alert />
     </>);
 }
--- assets/js/services/Config.js
+++ assets/js/services/Config.js
@@ -1,6 +1,6 @@
-export const API_URL = process.env.API_URL;
-export const LOGIN_API = API_URL + "login_check"
-export const API_PRODUCT = API_URL + "products"
-export const API_LOCATION = API_URL + "locations"
-export const API_CATEGORY = API_URL + "categories"
+export const API_URL=process.env.API_URL;
+export const LOGIN_API=API_URL+"login_check"
+export const API_PRODUCT=API_URL+"products"
+export const API_LOCATION=API_URL+"locations"
+export const API_CATEGORY=API_URL+"categories"
 
