0e34628b856dd761563a70242a4cc591fae9e9a0
Alpha Barry <barrymamadoualpha2@gmail.com>
2021-08-16 18:18:35 +0200

    resolution probleme de migration

--- migrations/Version20210805082112.php
+++ migrations/Version20210805082112.php
@@ -20,7 +20,9 @@ final class Version20210805082112 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE note_book');
+        $this->addSql('CREATE TABLE note_book (id INT AUTO_INCREMENT NOT NULL, product_id INT NOT NULL, marque VARCHAR(255) NOT NULL, model VARCHAR(255) NOT NULL, processor VARCHAR(255) NOT NULL, ram VARCHAR(255) NOT NULL, hdd VARCHAR(255) NOT NULL, grade VARCHAR(255) NOT NULL, category VARCHAR(255) NOT NULL, location VARCHAR(255) NOT NULL, price NUMERIC(5, 2) DEFAULT NULL, priceb2b VARCHAR(255) DEFAULT NULL, screen VARCHAR(255) NOT NULL, note LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+
+       # $this->addSql('DROP TABLE note_book');
         $this->addSql('ALTER TABLE note_book DROP category, DROP location, CHANGE priceb2b priceb2b NUMERIC(5, 2) DEFAULT NULL');
     }
 
